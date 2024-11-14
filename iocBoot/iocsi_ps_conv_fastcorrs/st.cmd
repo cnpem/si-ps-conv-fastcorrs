6d5eebf28ab7edd9ef7e34e70283e3f63c25b2d6
Gustavo de Souza dos Reis <gustavo.reis@lnls.br>
2024-11-14 14:41:13 -0300

    Remove user specific definitions.

--- iocBoot/iocsi_ps_conv_fastcorrs/st.cmd
+++ iocBoot/iocsi_ps_conv_fastcorrs/st.cmd
@@ -1,7 +1,6 @@
 #!../../bin/linux-x86_64/si_ps_conv_fastcorrs
 
 < envPaths
-epicsEnvSet("CALC","/home/ABTLUS/gustavo.reis/EPICS/support/calc")
 cd "${TOP}"
 
 ## Register all support components
@@ -9,7 +8,7 @@ dbLoadDatabase "dbd/si_ps_conv_fastcorrs.dbd"
 si_ps_conv_fastcorrs_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
-dbLoadRecords("db/dipole.db","user=gustavoreis, SEC=$(SEC)")
+dbLoadRecords("db/dipole.db","SEC=$(SEC)")
 dbLoadRecords("db/machine_params.db")
 
 cd "${TOP}/iocBoot/${IOC}"
